dfe029cb7548d053587da7262c1d47b57874be49
gratestas <stasgrate@gmail.com>
2023-02-22 00:11:22 +0300

    feat(web): add Web3Provider component to App rendering

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
 import { HashRouter as Router } from "react-router-dom";
+import Web3Provider from "./context/Web3Provider";
 
 const container = document.getElementById("app");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Web3Provider>
+        <App />
+      </Web3Provider>
     </Router>
   </React.StrictMode>
 );
