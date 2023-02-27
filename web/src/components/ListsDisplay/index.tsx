531c9a6dfbbda212f7689ad08cb43ee4ad9566bd
gratestas <stasgrate@gmail.com>
2023-02-27 13:32:17 +0300

    feat(web): add DataFetcher wrapper component to fetch data from ipfs

--- web/src/components/ListsDisplay/index.tsx
+++ web/src/components/ListsDisplay/index.tsx
@@ -1,27 +1,56 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import styled from "styled-components";
 
 import { Card } from "@kleros/ui-components-library";
 import ListRow from "./ListRow";
+import { useIPFSQuery } from "../../hooks/useIPFSQuery";
 
 export interface IItemInfo {
+  itemId: string;
   label: string;
   status: string;
   itemsCount?: number | string;
+  chainName?: string;
   url?: string;
+  logoURI?: string;
   Icon?: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
 }
 interface ListDisplayProps {
-  data: IItemInfo[];
+  data: any[];
 }
 
 const ListsDisplay: React.FC<ListDisplayProps> = ({ data }) => {
   return (
     <StyledCard>
-      {data &&
+      {data?.map((registry) => {
+        console.log({ registry });
+        return (
+          <DataFetcher
+            key={registry.id}
+            url={registry?.registrationMetaEvidence?.URI}
+          >
+            {(data) => {
+              if (!data) return <div>loading...</div>;
+              console.log("metadata", data?.metadata);
+              console.log("metadata logo", data?.metadata?.logoURI);
+              return (
+                <ListRow
+                  itemId={registry.id}
+                  label={data?.metadata?.tcrTitle}
+                  logoURI={data?.metadata?.logoURI}
+                  status="Included"
+                  itemsCount={registry?.numberOfRegistered}
+                  chainName={registry?.chainName}
+                />
+              );
+            }}
+          </DataFetcher>
+        );
+      })}
+      {/*  {data &&
         data.map(({ Icon, label, url, itemsCount, status }, index) => (
           <ListRow key={index} {...{ Icon, label, url, itemsCount, status }} />
-        ))}
+        ))} */}
     </StyledCard>
   );
 };
@@ -35,3 +64,14 @@ const StyledCard = styled(Card)`
   height: fit-content;
   font-size: 14px;
 `;
+
+interface DataFetcherProps {
+  children: (data: any) => ReactNode;
+  url: string;
+}
+
+const DataFetcher: React.FC<DataFetcherProps> = ({ url, children }) => {
+  const { data } = useIPFSQuery(url);
+
+  return <>{children(data)}</>;
+};
