8520b816fd75bb26b82dd7a5b6fdb5c82a8d1478
gratestas <stasgrate@gmail.com>
2023-02-27 13:30:32 +0300

    refactor(web): add additional props to display fetche data

--- web/src/components/ListsDisplay/ListRow.tsx
+++ web/src/components/ListsDisplay/ListRow.tsx
@@ -5,30 +5,50 @@ import { useLocation, useNavigate } from "react-router-dom";
 import LightButton from "components/LightButton";
 import StatusBadge from "components/StatusBadge";
 import Title from "components/Title";
-import { IItemInfo } from "components/ListsDisplay";
+import { IItemInfo } from "../../components/ListsDisplay";
 import { useBreadcrumbContext } from "hooks/useBreadcrumbContext";
 
+import EthereumIcon from "svgs/icons/ethereum.svg";
+import GnosisIcon from "svgs/icons/gnosis.svg";
+
+const NETWORK_ICONS = {
+  gnosis: GnosisIcon,
+  goerli: EthereumIcon,
+};
+
 const ListRow: React.FC<IItemInfo> = ({
+  itemId,
   Icon,
   label,
   url,
+  chainName,
+  logoURI,
   itemsCount,
   status,
 }) => {
   const navigate = useNavigate();
-  const { pathname } = useLocation();
+  let { pathname } = useLocation();
   const { addItem } = useBreadcrumbContext();
 
+  console.log({ pathname });
   const handleClick = () => {
     addItem(label);
-    navigate(`${pathname}/2`);
+    if (pathname === "/") pathname += "registry";
+    navigate(`${pathname}/${itemId}`);
   };
+
+  const NetworkIcon = chainName && NETWORK_ICONS[chainName];
   return (
     <StyledRow>
       <LeftSide>
-        <Title {...{ Icon, text: label, link: url }} />
+        <Title {...{ Icon, text: label, logoURI, link: url }} />
       </LeftSide>
       <RightSide>
+        {chainName && (
+          <IconWrapper>
+            <NetworkIcon />
+          </IconWrapper>
+        )}
         {itemsCount && <label>{itemsCount} Items</label>}
         <StatusBadge status={status} />
         <LightButton text="Open" onClick={handleClick} />
@@ -49,11 +69,11 @@ const StyledRow = styled.div`
 const RightSide = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-between;
   width: 30%;
   font-size: 14px;
   label {
-    width: 60px;
+    width: 70px;
   }
 `;
 
@@ -62,4 +82,15 @@ const LeftSide = styled.div`
   align-items: center;
   justify-content: space-between;
   flex: 1;
+`;
+
+const IconWrapper = styled.div`
+  width: 14px;
+  height: 14px;
+  display: flex;
+  align-items: center;
+  svg {
+    width: 100%:
+    height: auto;
+  }
 `;
